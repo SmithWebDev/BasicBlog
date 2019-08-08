ab4c82ece9cf475a2c53f054a40cfe4128d1c0e4
Smith <smithwebdd@gmail.com>
2019-08-07 21:07:48 -0500

    created Devise views and added username to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_023200) do
+ActiveRecord::Schema.define(version: 2019_08_08_020611) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_08_06_023200) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
