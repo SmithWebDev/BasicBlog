e3bd7cd47d3ca6d9715f943ce4e9ce585de0e5af
Smith <smithwebdd@gmail.com>
2019-08-07 20:33:57 -0500

    added Optional true value to belongs to associations in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :post, :optional => true
-  belongs_to :user, :optional => true
+  belongs_to :post, optional: true
+  belongs_to :user, optional: true
 
 end
